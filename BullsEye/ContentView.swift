687ded9368c85bbb820ffbcf3d026215daeafa70
Alex Perse <alexanderperse@gmail.com>
2019-12-07 14:05:51 -0500

    Finished SwiftUI tutorial project

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -15,6 +15,49 @@ struct ContentView: View {
   @State var target = Int.random(in: 1...100)
   @State var score = 0
   @State var round = 1
+    let midnightBlue = Color(red: 0.0 / 255.0, green: 51.0 / 255.0, blue: 102.0 / 255.0)
+
+    struct LabelStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.white)
+            .modifier(Shadow())
+            .font(Font.custom("Arial Rounded MT Bold", size: 18))
+        }
+    }
+    
+    struct ValueStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.yellow)
+            .modifier(Shadow())
+            .font(Font.custom("Arial Rounded MT Bold", size: 24))
+        }
+    }
+    
+    struct Shadow: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+        }
+    }
+    
+    struct ButtonLargeTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.black)
+            .font(Font.custom("Arial Rounded MT Bold", size: 18))
+        }
+    }
+    
+    struct ButtonSmallTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.black)
+            .font(Font.custom("Arial Rounded MT Bold", size: 12))
+        }
+    }
+
  
   var body: some View {
     VStack {
@@ -22,16 +65,16 @@ struct ContentView: View {
       
       // Target row
       HStack {
-        Text("Put the bullseye as close as you can to:")
-        Text("\(target)")
+        Text("Put the bullseye as close as you can to: ").modifier(LabelStyle())
+        Text("\(target)").modifier(ValueStyle())
       }
       Spacer()
       
       // Slider row
       HStack {
-        Text("1")
-        Slider(value: $sliderValue, in: 1...100)
-        Text("100")
+        Text("1").modifier(LabelStyle())
+        Slider(value: $sliderValue, in: 1...100).accentColor(Color.green)
+        Text("100").modifier(LabelStyle())
       }
       Spacer()
       
@@ -41,8 +84,9 @@ struct ContentView: View {
         self.alertIsVisible = true
         
       }) {
-        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/).modifier(ButtonLargeTextStyle())
       }
+    
       .alert(isPresented: $alertIsVisible) { () -> Alert in
         //let roundedValue = Int(sliderValue.rounded())
         return Alert(title: Text(alertTitle()), message: Text(
@@ -54,6 +98,7 @@ struct ContentView: View {
           self.round = self.round + 1
         })
       }
+      .background(Image("Button")).modifier(Shadow())
       Spacer()
       
       // Score row
@@ -61,21 +106,32 @@ struct ContentView: View {
         Button(action: {
             self.startGameOver()
         }) {
-          Text("Start Over")
+            HStack {
+            Image("StartOverIcon")
+            Text("Start Over").modifier(ButtonSmallTextStyle())
+            }
         }
+        .background(Image("Button")).modifier(Shadow())
         Spacer()
-        Text("Score:")
-        Text("\(score)")
+        Text("Score:").modifier(LabelStyle())
+        Text("\(score)").modifier(ValueStyle())
         Spacer()
-        Text("Round:")
-        Text("\(round)")
+        Text("Round:").modifier(LabelStyle())
+        Text("\(round)").modifier(ValueStyle())
         Spacer()
-        Button(action: {}) {
-          Text("Info")
+        NavigationLink(destination: AboutView()) {
+            HStack {
+                Image("InfoIcon")
+                Text("Info").modifier(ButtonSmallTextStyle())
+            }
         }
+        .background(Image("Button")).modifier(Shadow())
       }
       .padding(.bottom, 20)
     }
+    .background(Image("Background"), alignment: .center)
+    .accentColor(midnightBlue)
+  .navigationBarTitle("Bullseye")
   }
   
   func sliderValueRounded() -> Int {
@@ -118,6 +174,8 @@ struct ContentView: View {
     func startGameOver() {
         score = 0
         round = 1
+        sliderValue = 50.0
+        target = Int.random(in: 1...100)
     }
   
 }
